37f499e9e2d1862e9a0c5b61518aff38f0ad6ce6
benjokapu <144476747+benjokapu@users.noreply.github.com>
2024-05-27 09:03:23 -0300

    Check Password

--- Assets/Scripts/LogInManager.cs
+++ Assets/Scripts/LogInManager.cs
@@ -1,18 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LogInManager : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public InputField inputPassword;
+    public string RightPassword = "Benjo";
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    } 
+
+    public void CheckPassword()
+    {
+        if (inputPassword.text == RightPassword)
+        {
+            Debug.Log("Access Granted!");
+        }
+        else
+        {
+            Debug.Log("Access Denied");
+        }
     }
 }
